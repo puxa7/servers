3c1fe612c74b45821a573b24935d992a0efe284b
puxa7 <umpik123@gmail.com>
2026-03-10 17:24:08 +0100

    middleware

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import { handlerReadiness } from "./api/readiness.js";
+import { Request, Response, NextFunction } from "express";
+
+type Middleware = (req: Request, res: Response, next: NextFunction) => void;
 
 const app = express();
 const PORT = 8080;
@@ -19,3 +22,15 @@ app.get("/healthz", handlerReadiness);
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+const middlewareLogResponses: Middleware = (req: Request, res: Response, next: NextFunction) => {
+  res.on('finish', () => {
+    const statusCode = res.statusCode;
+    if (statusCode < 200 || statusCode >= 300){
+      console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${statusCode}`);
+    }
+  });
+  next();
+};
+
+app.use(middlewareLogResponses);
